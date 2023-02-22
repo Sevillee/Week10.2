1f547ac0e2d363742ee8108e0ea089838e5566ad
Anna Li <106352481+Sevillee@users.noreply.github.com>
2023-02-22 11:59:26 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let img1 = document.getElementById('img1')
+let img2 = document.getElementById('img2')
+
+function getPicture1(){
+    img1.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUUWaUYBaL3PJ-rnAD8fT_gGGpoNzFsBCcdA&usqp=CAU'
+}
+
+function getPicture2(){
+    img1.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ29bQT_PVcKkA8WTfG4RnqUbT_C8wkGrtnuA&usqp=CAU'
+}
